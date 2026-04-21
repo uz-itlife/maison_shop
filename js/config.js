285f4d25b066530062d783e44b8449be37118cb1
bdimuratovahrom <_email@gmail.com>
2026-04-21 18:24:38 +0500

    подключили2

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// js/config.js — этот файл НЕ загружается на GitHub
+const TELEGRAM_TOKEN = '8744858618:AAFFmHNmEKaBfJKoInwab85dgHnoc-ygJHE';
+const TELEGRAM_CHAT_ID = '320502463';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxKwipxtWlTMwXuwNM1Ev9ZIKUJMsWopibZayJerUygmAncJCDjmjyH8fmXy6Kwbzs4/exec';
